7b0d21c38073c0e6ab5a23645dceb1f6ac7c9663
Teboho Thapeli <tm.thapeli@gmail.com>
2021-07-03 16:07:52 +0200

    movie trailers

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import './App.css';
 import Row from './Row';
 import requests from './requests';
@@ -5,6 +6,11 @@ import Banner from './Banner';
 import Nav from './Nav';
 
 function App() {
+
+  useEffect(() => {
+    document.title = 'VidClip - Netflix Clone';
+  });
+
   return (
     <div className="app">
       <Nav/>
